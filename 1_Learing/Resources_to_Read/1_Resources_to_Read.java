87027bd1b9014c5c162cbac87367f7f4ee810997
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-02 20:36:44 +0300

    Create 1_Resources_to_Read.java

---
+++ 1_Learing/Resources_to_Read/1_Resources_to_Read.java
@@ -0,0 +1,62 @@
+To be a successful businessman, you need to have a good understanding of running businesses, corporate governance, financial systems, technology, and government influence on businesses. You also need to be able to operate corporate companies and manage finances and operations.
+
+Here are some books, courses, and videos to help you achieve this:
+
+**Books:**
+
+* **The Lean Startup** by Eric Ries
+* **Zero to One** by Peter Thiel
+* **Crossing the Chasm** by Geoffrey A. Moore
+* **Good Strategy/Bad Strategy** by Richard Rumelt
+* **Built to Last** by Jim Collins and Jerry I. Porras
+* **The Innovator's Dilemma** by Clayton M. Christensen
+* **The Hard Thing About Hard Things** by Ben Horowitz
+* **The Effective Executive** by Peter Drucker
+* **Team of Teams** by General Stanley McChrystal
+* **The Startup Owner's Manual** by Steve Blank
+* **The Art of Innovation** by Tom Kelley and David Kelley
+* **The Innovator's DNA** by Jeff Dyer, Hal Gregersen, and Clayton M. Christensen
+* **The Second Machine Age** by Erik Brynjolfsson and Andrew McAfee
+
+**Courses:**
+
+* **Coursera: Business Foundations**
+* **edX: Business and Management**
+* **Udemy: Business Administration**
+* **Udacity: Business Analytics**
+* **LinkedIn Learning: Business**
+* **Harvard Business School Online: Business Analytics**
+* **Wharton Online: Business Management**
+* **Stanford Online: Entrepreneurship and Innovation**
+* **MIT Sloan Executive Education: Business Strategy**
+* **London Business School Online: Corporate Finance**
+* **Kellogg School of Management Online: Financial Management**
+* **Columbia Business School Online: Marketing Management**
+
+**Videos:**
+
+* **TED Talks: Business**
+* **Khan Academy: Business and Economics**
+* **Crash Course: Business**
+* **The Startup Videos**
+* **Y Combinator: Startup School**
+* **TechCrunch: Startup Alley**
+* **Forbes: Entrepreneurs**
+* **Inc.: Starting a Business**
+* **Harvard Business Review: Business**
+* **Wharton Business School: Business**
+* **Stanford Graduate School of Business: Business**
+* **MIT Sloan Management Review: Business**
+* **Columbia Business School: Business**
+* **Kellogg School of Management: Business**
+
+In addition to the above, I recommend that you read business magazines and newspapers, such as The Economist, Forbes, and Inc. You should also attend industry events and conferences to network with other businesspeople and learn about the latest trends and developments.
+
+It is also important to find a mentor or coach who can guide you and help you learn from their experience.
+
+Finally, don't be afraid to fail. Failure is a natural part of the learning process. The important thing is to learn from your mistakes and keep moving forward.
+
+I hope this helps!
+
+
+  ......
